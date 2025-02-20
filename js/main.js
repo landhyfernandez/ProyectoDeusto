5ac0518d916051e4f8e0bee910288b7fb28ac037
landhy <landhyf@gmail.com>
2025-02-20 21:02:03 +0100

    add chainingOperator

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
-console.log('Hola Mundo');
+const user1 = {
+    name: 'Juan',
+    surname: 'Perez Lopez',
+    address: {
+        street: 'Principe de Vergara, 10',
+        city: 'Madrid'
+    }
+}
+
+const user2 = {
+    name: 'Laura',
+    surname: 'Nadal Novo',
+}
+
+let city2;
+if (user2.address) {
+    city2 = user2.address.city;
+}
+console.log(city2);
